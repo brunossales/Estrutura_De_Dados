d1976211c810d0b9b08b6949c22eda9e75261911
brunossales <bruno.particular25@alu.ufc.br>
2021-06-04 13:28:13 -0300

    adicionando os códigos em C++ da cadeira de estrutura de dados feitos até o momento

---
+++ Métodos de Ordenação/Insertion Sort/InsertionSort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 100000;
+void insertion(int vetor[], int size)
+{
+
+    for (int i = 0; i < size - 1; i++)
+        for (int j = i + 1; j > 0; j--)
+        {
+            if (vetor[j] < vetor[j - 1])
+            {
+                int aux = vetor[j];
+                vetor[j] = vetor[j - 1];
+                vetor[j - 1] = aux;
+            }
+            else
+                break;
+        }
+};
+
+int main()
+{
+    int v[MAX];
+    int size;
+    cout << "Digite o tamanho do vetor" << endl;
+    cin >> size;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Digite o valor da posicao - " << i << endl;
+        cin >> v[i];
+    }
+    insertion(v, size);
+    cout << endl
+         << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << v[i] << endl;
+    }
+}
---
+++ Métodos de Ordenação/Selection Sort/SelectionYouTube.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void selecao(int vet[], int n)
+{
+    int menor;
+    for (int i = 0; i < n - 1; i++)
+    {
+        menor = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (vet[menor] > vet[j])
+                menor = j;
+        }
+        if (i != menor)
+        {
+            int aux = vet[i];
+            vet[i] = vet[menor];
+            vet[menor] = aux;
+        }
+    }
+};
+int main()
+{
+    int n = 10;
+    int v[n] = {3, 6, 5, 1, 2, 8, 7, 9, 4, 10};
+    selecao(v, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << endl;
+    }
+
+    system("pause");
+}
