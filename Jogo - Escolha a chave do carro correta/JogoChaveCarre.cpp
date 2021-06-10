8c5e46ed7b41d7c642d3f3c3d8c7133ca6236dbd
brunossales <bruno.particular25@alu.ufc.br>
2021-06-10 09:12:38 -0300

    modifiqued game

--- Jogo - Escolha a chave do carro correta/JogoChaveCarre.cpp
+++ Jogo - Escolha a chave do carro correta/JogoChaveCarre.cpp
@@ -3,3 +3,37 @@
 #include <cstdlib>
 
 using namespace std;
+int numeroAleatorio(int menor, int maior)
+{
+    return rand() % (maior - menor + 1) + menor;
+};
+void ajuda()
+{
+    cout << "O jogo acontece conforme o numero de chaves que voce escolheu no comeco" << endl;
+    cout << "voce deve digitar jogar, para escolher uma chave aleatoria" << endl;
+    cout << "Se a chave ligar voce ganha" << endl;
+    cout << "Se nao ligar voce perde essa rodada, voce pode no maximo errar 3 vezes" << endl;
+}
+class Jogo
+{
+    int cha;
+
+public:
+    Jogo();
+    Jogo(int a)
+    {
+        cha = a;
+    }
+};
+
+int main()
+{
+    int chaves = 0;
+    while (chaves < 5)
+    {
+        cout << "Digite o numero de chaves" << endl;
+        cin >> chaves;
+        if (chaves < 5)
+            cout << "Tem que ser maior ou igual a 5";
+    }
+}
