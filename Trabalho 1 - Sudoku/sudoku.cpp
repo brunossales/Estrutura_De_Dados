5673a9190349a414cdae51282af7f752cda47e42
brunossales <bruno.particular25@alu.ufc.br>
2021-06-14 12:21:19 -0300

    adicionando o trabalho ainda incompleto

--- Trabalho 1 - Sudoku/sudoku.cpp
+++ Trabalho 1 - Sudoku/sudoku.cpp
@@ -1,20 +1,46 @@
 #include <iostream>
 #include <string.h>
+#include <stdlib.h>
+#include <ctime>
 using namespace std;
 
-class puzzle
+int pegaproximo(int possiveis[])
+{
+    int temApenaszeros = 1;
+    for (int i = 0; i < 9; i++)
+    {
+        if (possiveis[i])
+            temApenaszeros = 0;
+    }
+    if (temApenaszeros)
+        return 0;
+    srand(unsigned(time(0)));
+    int valor = 0;
+    while (true)
+    {
+        int i = rand() % 9;
+        valor = possiveis[i];
+        if (valor)
+        {
+            possiveis[i] = 0;
+            break;
+        }
+    }
+    return valor;
+};
+class sudokuu
 {
 private:
-    quadrado *celulas[3][3];
-
 public:
-    puzzle()
+    quadrado *celulas[3][3];
+    sudokuu()
     {
+
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
                 this->celulas[i][j] = new quadrado();
     }
-    ~puzzle();
+    ~sudokuu();
     bool valida()
     {
         bool achou = true;
@@ -33,9 +59,10 @@ private:
 public:
     quadrado()
     {
+        int numeross[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         for (int i = 0; i < 3; i++)
             for (int j = 0; j < 3; j++)
-                this->numbers[i][j] = 0;
+                this->numbers[i][j] = pegaproximo(numeross);
     }
     ~quadrado();
     // verificar se o quadrado é valido
@@ -58,10 +85,12 @@ public:
 };
 int main(int argc, char *args[]) //argc é a contagem de nomes que vamos ter na nossa entrada
 {                                // *args é o proprio valor que digitamos, lembrando que o ./sudoku.exe é a posição 0
-    if (strcmp(argc[1], "--criar") == 0)
+    if (strcmp(args[1], "--criar") == 0)
     {
         int n;
         sscanf(args[2], "%d", &n);
+        sudokuu sudoku();
+        cout << sudoku;
     }
 
     return 0;
