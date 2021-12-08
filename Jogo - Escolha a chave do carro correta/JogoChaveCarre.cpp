5abfc7322e712d6d3ae425e2363a4e67ec5f56ce
brunossales <bruno.particular25@alu.ufc.br>
2021-12-08 19:54:48 -0300

    updates

--- Jogo - Escolha a chave do carro correta/JogoChaveCarre.cpp
+++ Jogo - Escolha a chave do carro correta/JogoChaveCarre.cpp
@@ -23,7 +23,7 @@ public:
     void ajuda()
     {
         cout << "# O jogo acontece conforme o numero de chaves que voce escolheu no comeco" << endl;
-        cout << "# voce deve digitar jogar, para escolher uma chave aleatoria" << endl;
+        cout << "# voce deve digitar 1, para escolher uma chave aleatoria" << endl;
         cout << "# Se a chave ligar voce ganha" << endl;
         cout << "# Se nao ligar voce perde essa rodada, voce pode no maximo errar 3 vezes" << endl
              << endl;
@@ -65,7 +65,7 @@ int main()
         cout << "Digite o numero de chaves" << endl;
         cin >> chaves;
         if (chaves < 5)
-            cout << "Tem que ser maior ou igual a 5";
+            cout << "Tem que ser maior ou igual a 5!\n";
     }
     Jogo *jogo = new Jogo(chaves);
     while (true)
