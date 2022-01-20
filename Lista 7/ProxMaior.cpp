95eee4110a4f62d77de02477c15a9ce1211b4958
brunossales <bruno.particular25@alu.ufc.br>
2022-01-20 12:16:29 -0300

    quest 1 and 2 completed

---
+++ Lista 7/ProxMaior.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+  int n, value;
+  stack<int> inteiros;
+  stack<int> indexs;
+  vector<int> vector;
+  cin >> n;
+  for (int i = 0; i < n; i++)
+  {
+    cin >> vector[i];
+    if (i == 0)
+      indexs.push(i);
+    else
+    {
+    }
+  }
+}
