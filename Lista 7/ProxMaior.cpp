84bb8f00f365cd1d813618766dcce2a74d123e7f
brunossales <bruno.particular25@alu.ufc.br>
2022-01-20 21:03:45 -0300

    question 3  finished

--- Lista 7/ProxMaior.cpp
+++ Lista 7/ProxMaior.cpp
@@ -1,21 +1,57 @@
 #include <iostream>
 #include <stack>
+#include <vector>
 using namespace std;
 
 int main()
 {
   int n, value;
-  stack<int> inteiros;
+  vector<int> inteiros, printt;
   stack<int> indexs;
-  vector<int> vector;
   cin >> n;
+  printt.resize(n);
   for (int i = 0; i < n; i++)
   {
-    cin >> vector[i];
+    cin >> value;
+    inteiros.push_back(value);
+  }
+  // 2 9 8
+  for (int i = 0; i < n; i++)
+  {
     if (i == 0)
       indexs.push(i);
     else
     {
+      if (inteiros[i] > inteiros[indexs.top()])
+      {
+        while (!indexs.empty())
+        {
+          if (inteiros[i] > inteiros[indexs.top()])
+          {
+            printt[indexs.top()] = i;
+            indexs.pop();
+          }
+          else
+            break;
+        }
+        indexs.push(i);
+      }
+      else
+        indexs.push(i);
     }
+  }
+  while (!indexs.empty())
+  {
+    printt[indexs.top()] = -1;
+    indexs.pop();
+  }
+  for (int i = 0; i < n; i++)
+  {
+    if (i == n - 1)
+    {
+      cout << printt[i] << endl;
+      break;
+    }
+    cout << printt[i] << " ";
   }
 }
